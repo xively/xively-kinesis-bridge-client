ba115fe6fcedbd79c705d672813d0963dfc5707a
Gabriel Rilo <gabriel@nimacloud.com>
2017-04-02 20:33:49 -0300

    Removing build files

--- src/kinesis-bridge-envelope.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-var KinesisBridgeEnvelope = (function () {
-    function KinesisBridgeEnvelope() {
-    }
-    return KinesisBridgeEnvelope;
-}());
-exports.KinesisBridgeEnvelope = KinesisBridgeEnvelope;
